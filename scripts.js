f65345c1b2b387ffceaeafcbaac3b6b3a471d01b
earosselot <earosselot@gmail.com>
2021-02-03 01:57:59 -0300

    modal for new book added. Lacks the form to create new book.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 let myLibrary = []
+const libraryTable = document.querySelector('#libraryTable')
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+function Book (title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+};
 
 Book.prototype.info = function() {
     let readedString;
@@ -18,5 +19,27 @@ Book.prototype.info = function() {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
+
+addBookToLibrary('Hobbit', 'Tolkien', 540, true);
+addBookToLibrary('harry Potter', 'Rowling', 246, true);
+addBookToLibrary('caperucita', 'Anonimo', 30, false);
+
+console.log(myLibrary);
+console.table(myLibrary);
+
+
+myLibrary.forEach(book => {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    Object.keys(book).forEach(key => {
+        let col = document.createElement('div');
+        col.classList.add('col');
+        col.textContent = book[key];
+        row.appendChild(col);
+    });
+    libraryTable.appendChild(row);
+});
+
